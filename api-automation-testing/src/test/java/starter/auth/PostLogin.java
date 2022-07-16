e217d33990cb5f3321463a022580db71f8e94391
radenraechan <rradity2500@gmail.com>
2022-07-16 23:04:49 +0700

    feat: update auth feature

--- api-automation-testing/src/test/java/starter/auth/PostLogin.java
+++ api-automation-testing/src/test/java/starter/auth/PostLogin.java
@@ -4,13 +4,10 @@ import utils.General;
 import io.restassured.response.Response;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
-import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONObject;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 import static utils.General.base_url;
 import static org.hamcrest.Matchers.equalTo;
--- api-automation-testing/src/test/java/starter/stepdefinitions/AuthSteps.java
+++ api-automation-testing/src/test/java/starter/stepdefinitions/AuthSteps.java
@@ -5,36 +5,48 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
-import starter.auth.PostLogin;
-import starter.auth.PostRegister;
+import starter.auth.*;
+
+import java.io.IOException;
 
 public class AuthSteps {
+
+    @Steps
+    PutProfile putProfile;
+
+    @Steps
+    PostRegister postRegister;
 
     @Steps
     PostLogin postLogin;
 
     @Steps
-    PostRegister postRegister;
+    PostProfileImage postProfileImage;
 
-    @Given("I set an endpoint for POST login")
-    public void iSetAnEndpointForPOSTLogin() {
-        postLogin.setPostEndpointLogin();
+    @Steps
+    GetProfileImage getProfileImage;
+
+    @Steps
+    GetProfile getProfile;
+
+    @Given("I set an endpoint for PUT profile")
+    public void iSetAnEndpointForPUTProfile() {
+        putProfile.setPUTEndpointProfile();
     }
 
-    @When("I request POST detail login with input {string} and {string}")
-    public void iRequestPOSTDetailLoginWithInputAnd(String email, String password) throws Exception {
-        postLogin.requestPostDetailLogin(email, password);
+    @When("I request PUT  detail profile with {string}, input {string}, {string}, {string}, and {string}")
+    public void iRequestPUTDetailProfileWithInputAnd(String status, String fname, String lname, String phone, String email) throws Exception {
+        putProfile.requestPUTDetailProfile(status, fname, lname, phone, email);
     }
 
-    @Then("I validate the status code is {int}")
-    public void iValidateTheStatusCodeIsStatusCode(int statusCode) {
-        postLogin.validateStatusCode(statusCode);
-        postRegister.validateStatusCode(statusCode);
+    @Then("I validate the status code for put profile is {int}")
+    public void iValidateTheStatusCodeForPutProfileIsStatusCode(int statusCode) {
+        putProfile.validateStatusCode(statusCode);
     }
 
-    @And("validate the data details and the {string} after login")
-    public void validateTheDataDetailsAndTheAfterLogin(String message) {
-        postLogin.validateDataDetailAfterLogin(message);
+    @And("validate the data details and the {string} after edit profile")
+    public void validateTheDataDetailsAndTheAfterEditProfile(String message) {
+        putProfile.validateDataDetailAfterEditProfile(message);
     }
 
     @Given("I set an endpoint for POST register")
@@ -47,8 +59,89 @@ public class AuthSteps {
         postRegister.requestPostDetailRegister(fname, lname, phone, email, password);
     }
 
+    @Then("I validate the status code is {int}")
+    public void iValidateTheStatusCodeIsStatusCode(int statusCode) {
+        postLogin.validateStatusCode(statusCode);
+        postRegister.validateStatusCode(statusCode);
+    }
+
     @And("validate the data details and the {string} after register")
     public void validateTheDataDetailsAndTheAfterRegister(String message) {
         postRegister.validateDataDetailAfterRegister(message);
+    }
+
+    @Given("I set an endpoint for POST login")
+    public void iSetAnEndpointForPOSTLogin() {
+        postLogin.setPostEndpointLogin();
+    }
+
+    @When("I request POST detail login with input {string} and {string}")
+    public void iRequestPOSTDetailLoginWithInputAnd(String email, String password) throws Exception {
+        postLogin.requestPostDetailLogin(email, password);
+    }
+
+    @And("validate the data details and the {string} after login")
+    public void validateTheDataDetailsAndTheAfterLogin(String message) {
+        postLogin.validateDataDetailAfterLogin(message);
+    }
+
+    @Given("I set an endpoint for POST profile image")
+    public void iSetAnEndpointForPOSTProfileImage() {
+        postProfileImage.setPostEndpointProfileImage();
+    }
+
+    @When("I request POST detail profile image with {string} and input {string}")
+    public void iRequestPOSTDetailProfileImageWithAndInput(String status, String image) throws IOException {
+        postProfileImage.requestPostDetailProfileImage(status, image);
+    }
+
+    @Then("I validate the status code for post profile image is {int}")
+    public void iValidateTheStatusCodeForPostProfileImageIsStatusCode(int statusCode) {
+        postProfileImage.validateStatusCode(statusCode);
+    }
+
+    @And("validate the {string} and data details after post profile image")
+    public void validateTheAndDataDetailsAfterPostProfileImage(String message) {
+        postProfileImage.validateDataDetailAfterPostProfileImage(message);
+    }
+
+    @Given("I set an endpoint for GET profile image")
+    public void iSetAnEndpointForGETProfileImage() {
+        getProfileImage.setGetEndpointProfileImage();
+    }
+
+    @When("I request GET detail profile image with input {string}")
+    public void iRequestGETDetailProfileImageWithInput(String filename) throws IOException {
+        getProfileImage.requestGetDetailProfileImage(filename);
+    }
+
+    @Then("I validate the status code for get profile image is {int}")
+    public void iValidateTheStatusCodeForGetProfileImageIsStatusCode(int statusCode) {
+        getProfileImage.validateTheStatusCode(statusCode);
+    }
+
+    @And("validate the {string} and data profile image")
+    public void validateTheAndDataProfileImage(String message) {
+        getProfileImage.validateTheDataDetailsOfProfileImage(message);
+    }
+
+    @Given("I set an endpoint for GET profile")
+    public void iSetAnEndpointForGETProfile() {
+        getProfile.setGetEndpointProfile();
+    }
+
+    @When("I request GET detail profile with {string} and {string}")
+    public void iRequestGETDetailProfileWithAnd(String status, String token) throws IOException {
+        getProfile.requestGetDetailProfile(status, token);
+    }
+
+    @Then("I validate the status code for get profile is {int}")
+    public void iValidateTheStatusCodeForGetProfileIs(int statusCode) {
+        getProfile.validateTheStatusCode(statusCode);
+    }
+
+    @And("validate the {string} and data details of profile")
+    public void validateTheAndDataDetailsOfProfile(String message) {
+        getProfile.validateTheDataDetailsOfProfile(message);
     }
 }
